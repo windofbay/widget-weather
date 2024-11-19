ec61d5241d68fb8a71e84c9961a8a933d4cad541
Bayu Abdillah <bayupauli@gmail.com>
2024-11-19 11:30:10 +0700

    modif service

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -4,6 +4,7 @@ import { catchError, Observable } from 'rxjs';
 import { Weather } from './weather.model';
 import { HttpClient } from '@angular/common/http';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,16 +12,27 @@ export class WeatherService {
 
   constructor() { }
   private apiUrl = environment.apiUrl;
+  private placesUrl = environment.placesUrl;
+
   private http = inject(HttpClient);
   
   getWeather():Observable<any>{
-    return this.http.get<any>(`${this.apiUrl}?adm4=31.75.10.1001`)
+    return this.http.get<any>(`${this.apiUrl}?adm4=31.73.05.1005`)
     .pipe(
       catchError((err)=>{throw new Error(err)})
     );
   }
-  async getWeatherByLocation(location:string):Promise<Observable<any>>{
+  async getWeatherByCity(location:string):Promise<Observable<any>>{
     return await this.http.get<any>(`${this.apiUrl}?adm2=${location}`)
+  }
+  async getWeatherByVillage(location:string):Promise<Observable<any>>{
+    return await this.http.get<any>(`${this.apiUrl}?adm3=${location}`)
+  }
+  getProvinces():Observable<any>{
+    return this.http.get<any>(`${this.placesUrl}/provinces.json`)
+  }
+  getCities(provinceCode:string):Observable<any>{
+    return this.http.get<any>(`${this.placesUrl}/regencies/${provinceCode}.json`)
   }
   getTime(datetime:string):string{
     // Create a Date object with the specified time
@@ -42,5 +54,11 @@ export class WeatherService {
     weatherIcons.set("Moderate Rain","https://api-apps.bmkg.go.id/storage/icon/cuaca/hujan sedang-am.svg");  
     weatherIcons.set("Partly Cloudy","https://api-apps.bmkg.go.id/storage/icon/cuaca/cerah berawan-am.svg");  
     return weatherIcons.get(condition)!;
+  }
+  floorToClosestNumber(input: number, numbers: number[]): number | undefined {
+    // Filter the array to find all numbers less than or equal to the input
+    const possibleNumbers = numbers.filter(num => num <= input);
+    // Return the maximum of the filtered numbers, or undefined if no such number exists
+    return Math.max(...possibleNumbers);
   }
 }
